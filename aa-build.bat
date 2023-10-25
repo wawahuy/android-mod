a484856fed81c93c3545ffe315b756092b7e5023
wawahuy <kakahuy99@gmail.com>
2023-10-25 06:55:23 -0700

    add script

--- aa-build.bat
+++
@@ -1,7 +0,0 @@
-
-
-java -jar D:\apktool_2.9.0.jar b D:\apk\heo\smali -o build.apk
-
-jarsigner -storepass "adadad" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore D:\keys\my-release-key.keystore D:\apk\heo\build.apk alias_name
-
-pause
--- aa-push-lib.bat
+++
@@ -1,7 +0,0 @@
-adb shell am force-stop com.aladinfun.clashofsky_th_pig
-
-adb push D:\mod\app\build\intermediates\stripped_native_libs\release\out\lib\arm64-v8a\libmod.so /data/app/com.aladinfun.clashofsky_th_pig-UBJjeu0O3NTPLE_RNdrp7w==/lib/arm64/libmod.so
-
-adb shell monkey -p com.aladinfun.clashofsky_th_pig -c android.intent.category.LAUNCHER 1
-
-pause
