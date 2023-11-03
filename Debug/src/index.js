e157e1e66f8874e9b08e74d94eeb666cfe9d1c57
wawahuy <kakahuy99@gmail.com>
2023-11-03 10:29:36 -0700

    done send/recv frame encrypt socket

--- Debug/src/index.js
+++ Debug/src/index.js
@@ -1,13 +1,91 @@
 
 import net from 'net';
+import crypto from  'crypto';
 
 const server = net.createServer();
 server.listen(1235);
 
+const fin = {
+    end: 0x01,
+    continue: 0x00,
+};
+
+const opcode = {
+    json: 0b00000001,
+};
+
+const frameSize = 1024;
+const headerSize = 32; // fin opcode 2size 16iv 12pad ....
+
+const key = Buffer.from([0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8]);
+
+function sendJson(socket, json) {
+    const dataMaxSize = frameSize - headerSize; 
+    const dataStr = JSON.stringify(json);
+
+    const data = Buffer.alloc(dataStr.length + 1);
+    data.write(dataStr, 'utf-8');
+    data.writeUInt8(0, dataStr.length);
+
+    const dataLength = data.length;
+
+    let f = fin.continue;
+    for (let i = 0; i < dataLength; i += dataMaxSize) {
+        let e = i + dataMaxSize;
+        if (e >= dataLength) {
+            e = dataLength;
+            f = fin.end;
+        }
+
+        let sub = data.subarray(i, e);
+
+        const iv = Buffer.alloc(16);
+        const header = Buffer.alloc(headerSize);
+        header[0] = f;
+        header[1] = opcode.json;
+        header.writeInt16LE(sub.length, 2);
+        
+        for(let i = 0; i < 16; i++) {
+            header.writeInt8(Math.floor(Math.random() * 125), i + 4);
+            iv.writeInt8(header.at(i + 4), i);
+        }
+
+        console.log(header)
+        socket.write(header);
+
+        if (f == fin.end) {
+            const tmp = sub;
+            sub = Buffer.alloc(dataMaxSize);
+            tmp.copy(sub);
+        }
+
+        const cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
+        cipher.setAutoPadding(false);
+        let encryptedData = cipher.update(sub);
+        console.log(encryptedData);
+
+        const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+        decipher.setAutoPadding(false);
+        let decryptedData = Buffer.concat([decipher.update(encryptedData), decipher.final()]);
+        console.log('Old Data (Buffer):', sub);
+        console.log('Decrypted Data (Buffer):', decryptedData);
+
+        socket.write(encryptedData);
+    }
+}
+
 server.on('connection', (socket) => {
-    socket.write("abc");
-    socket.write(Buffer.alloc(1021));
-    console.log(socket);
+    sendJson(socket, { oq: true });
+    // socket.write(Buffer.from([
+    //     fin.end,
+    //     opcode.json,
+    //     ...keys
+    // ]));
+
+    // socket.write(Buffer.alloc(1017));
+    // console.log(socket);
+
+    // sendJson(socket, Array.from({length: 2000 }).map(() => ({ test: 1 })));
 });
 
 
--- PigMod/app/src/main/cpp/src/socket.h
+++ PigMod/app/src/main/cpp/src/socket.h
@@ -10,9 +10,23 @@
 #include <iostream>
 #include <unordered_map>
 #include <arpa/inet.h>
+#include <memory>
+
+#include "json.hpp"
+using namespace nlohmann;
+
+#define OFFSET_FIN 0
+#define OFFSET_OPCODE 1
+#define OFFSET_SIZE 2
+#define OFFSET_IV 4
+#define HEADER_SIZE 32
+
+#define BYTE_FIN 0x01
+#define BYTE_OPCODE_JSON 0b00000001
 
 namespace Socket {
     static int clientSocket = -1;
+    static uint8_t key[] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8,0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8};
 
     void init();
 
@@ -46,12 +60,58 @@ namespace Socket {
         char buffer[SOCKET_BUFFER_SIZE];
         int receivedBytes = -1;
         int totalBytesReceived = 0;
+
+        int offsetAllBytesReceived = 0;
+        char *allBytesReceived = 0;
+
         do {
             receivedBytes = recv(clientSocket, buffer + totalBytesReceived, SOCKET_BUFFER_SIZE - totalBytesReceived, 0);
             totalBytesReceived += receivedBytes;
             if (totalBytesReceived == SOCKET_BUFFER_SIZE) {
-                LOG_E("%s", buffer);
+                LOG_E("frame");
                 totalBytesReceived = 0;
+                char byteFin = buffer[OFFSET_FIN];
+                char byteOpcode = buffer[OFFSET_OPCODE];
+                short byteSize = *(short *)(buffer + OFFSET_SIZE);
+                uint8_t *iv = ((uint8_t *)buffer) + OFFSET_IV;
+                void *data = buffer + HEADER_SIZE;
+                bool fin = byteFin & BYTE_FIN;
+
+                if (offsetAllBytesReceived == 0) {
+                    allBytesReceived = (char *) malloc(byteSize);
+                } else {
+                    allBytesReceived = (char *) realloc(allBytesReceived, offsetAllBytesReceived + byteSize);
+                }
+
+                LOG_E("enc %p %p", *(unsigned char*)(data), *(unsigned char*)( (uintptr_t)data + 1));
+                struct AES_ctx ctx;
+                AES_init_ctx_iv(&ctx, key, iv);
+                AES_CBC_decrypt_buffer(&ctx, (uint8_t*)data, SOCKET_BUFFER_SIZE - HEADER_SIZE);
+                LOG_E("%s", data);
+
+                memcpy(allBytesReceived + offsetAllBytesReceived, data, byteSize);
+                offsetAllBytesReceived += byteSize;
+
+                if (fin) {
+                    LOG_E("end %p", byteOpcode);
+                    if (byteOpcode == BYTE_OPCODE_JSON) {
+                        LOG_E("iv %p %p %p %p", iv[0], iv[1], iv[6], iv[7]);
+                        LOG_E("json size: %i %p", offsetAllBytesReceived, *allBytesReceived);
+
+                        try
+                        {
+                            json js = json::parse(allBytesReceived);
+                            LOG_E("JSON Parse OKE");
+                        } catch (json::parse_error& ex) {
+                            LOG_E("Err: %i", ex.byte);
+                        }
+
+                        free(allBytesReceived);
+                        offsetAllBytesReceived = 0;
+                    }
+                } else {
+                    LOG_E("continue size: %i", byteSize);
+                }
             }
         } while (receivedBytes > 0);
 
