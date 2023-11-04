0451a21de7ff97f0a1f254d310dea96c8576f1e3
wawahuy <kakahuy99@gmail.com>
2023-11-03 22:51:15 -0700

    update scipt device real

---
+++ GameHeoDenRoi/aa-push-lib-dv.bat
@@ -0,0 +1,10 @@
+adb shell am force-stop com.aladinfun.clashofsky_th_pig
+
+echo f | xcopy /f /Y ..\PigMod\app\build\intermediates\stripped_native_libs\release\out\lib\arm64-v8a\libpigmod.so libpigmod.so
+echo f | xcopy /f /Y ..\PigMod\app\build\intermediates\dex\release\mergeDexRelease\classes.dex classes3.dex
+
+adb push ..\PigMod\app\build\intermediates\stripped_native_libs\release\out\lib\arm64-v8a\libpigmod.so /storage/emulated/0/libpigmod.so
+adb shell "su -c mv /storage/emulated/0/libpigmod.so /data/app/com.aladinfun.clashofsky_th_pig-*/lib/arm64/libpigmod.so"
+adb shell "su -c chmod 755 /data/app/com.aladinfun.clashofsky_th_pig-*/lib/arm64/libpigmod.so"
+
+adb shell monkey -p com.aladinfun.clashofsky_th_pig -c android.intent.category.LAUNCHER 1
