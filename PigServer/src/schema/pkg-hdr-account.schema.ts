c0309af343ae4eb7dc6c1a1c869b389e1307881b
wawahuy <kakahuy99@gmail.com>
2023-11-24 17:08:39 +0000

    new update

---
+++ PigServer/src/schema/pkg-hdr-account.schema.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, ObjectId, SchemaTypes, Types } from 'mongoose';
+
+export type PkgHdrAccountDocument = HydratedDocument<PkgHdrAccount>;
+
+@Schema({
+  timestamps: true,
+})
+export class PkgHdrAccount {
+  @Prop({ type: Types.ObjectId })
+  gameKeyId: ObjectId;
+
+  @Prop()
+  mtkey: string;
+
+  @Prop()
+  skey: string;
+
+  @Prop({ unique: true })
+  uid: string;
+
+  @Prop()
+  stsAllTili: number;
+
+  @Prop()
+  stsAllSnowball: number;
+
+  @Prop()
+  stsDayTili: number;
+
+  @Prop()
+  stsDaySnowball: number;
+}
+
+export const PkgHdrAccountSchema = SchemaFactory.createForClass(PkgHdrAccount);
